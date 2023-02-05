c8d213c11040b4dfa98dc21e2015f4c307802d18
lwalik <l.walicki1@gmail.com>
2023-02-05 16:53:49 +0100

    feat: stores with checkboxes added

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -4,9 +4,12 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { map, Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
 import { CategoryModel } from '../../models/category.model';
+import { StoreModel } from '../../models/store.model';
 import { CategoriesService } from '../../services/categories.service';
+import { StoresService } from '../../services/stores.service';
 
 @Component({
   selector: 'app-products',
@@ -24,12 +27,30 @@ export class ProductsComponent {
       tap(console.log),
       map((form) => Object.keys(form).filter((k) => form[k] === true))
     );
+  readonly stores$: Observable<StoreModel[]> = this._storesService
+    .getAll()
+    .pipe(tap((stores) => this.addControlsToStoresForm(stores)));
+  readonly storesForm: FormGroup = new FormGroup({});
+  readonly storesFormValue$: Observable<string[]> =
+    this.storesForm.valueChanges.pipe(
+      tap(console.log),
+      map((form) => Object.keys(form).filter((k) => form[k] === true))
+    );
 
-  constructor(private _categoriesService: CategoriesService) {}
+  constructor(
+    private _categoriesService: CategoriesService,
+    private _storesService: StoresService
+  ) {}
 
   addControlsToCategoriesForm(categories: CategoryModel[]): void {
     categories.forEach((c) => {
       this.categoriesForm.addControl(c.id, new FormControl(false));
+    });
+  }
+
+  addControlsToStoresForm(stores: StoreModel[]): void {
+    stores.forEach((s) => {
+      this.storesForm.addControl(s.id, new FormControl(false));
     });
   }
 }
